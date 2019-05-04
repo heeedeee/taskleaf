1961d9bb217add92bc3e4e6b334e7e86b7003dab
sugioka hidekazu <h.sugioka0708@gmail.com>
2019-05-04 21:44:20 +0900

    削除ボタンの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
-    
+
   end
 
   def new
@@ -26,7 +26,13 @@ class TasksController < ApplicationController
   def update
     task = Task.find(params[:id])
     task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました"
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+  end
+
+  def destory
+    task = Task.find(params[:id])
+    task.destory
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
   private
